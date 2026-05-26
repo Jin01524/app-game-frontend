934a95102394a61d5639238696e2ebf53dd88c9b
Jin01524 <ngocbinh1999@gmail.com>
2026-05-27 01:25:10 +0700

    feat: always-visible install button in user list

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+// Unlock screen rotation for PWA on Android
+if (screen.orientation && screen.orientation.unlock) {
+  screen.orientation.unlock();
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
